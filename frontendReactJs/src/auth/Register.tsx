0a65af16e2693635a559e089ecb19c7107667ea5
dipesh <dipeshpaharai@gmail.com>
2025-03-25 11:38:45 +0545

    register too in sync

--- frontendReactJs/src/auth/Register.tsx
+++ frontendReactJs/src/auth/Register.tsx
@@ -1,31 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaEye, FaGoogle } from "react-icons/fa";
-import { useSelector } from "react-redux";
-import { RootState } from "../redux/Store";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../redux/Store";
+import { useLoginMutation, useRegisterMutation } from "../redux/api/apiSlice";
+import { offRegisterButton } from "../redux/slices/ToggleRegisterSlice";
 
 const Register: React.FC = () => {
   const isRegisterToggled = useSelector((state: RootState) => state.toggleRegister.isRegisterToggled);
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [fullName, setUsername] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+
+  const dispatchRegister: AppDispatch = useDispatch();
+
+  const [Register, {isLoading, error, isError}] = useRegisterMutation();
+
+  const handleLogin= async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const result = await Register({email, password, fullName , confirmPassword}).unwrap();
+      console.log("User registered Successfully!!", result);
+      dispatchRegister(offRegisterButton());
+    } catch (error) {
+      console.log("Error Logging!!", error);
+      console.log(email, password, fullName, confirmPassword);
+      
+      
+    }
+  }
+
+
 
   return (
     <div className={`z-10 absolute transition-all duration-300 ease-in-out border-2 min-h-screen flex justify-center items-center ${isRegisterToggled ? 'block opacity-100' : 'hidden opacity-0'} mx-auto w-full overflow-hidden`}>
       <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-sm text-left"> {/* Reduced padding and width */}
         <h2 className="text-xl font-semibold mb-4">Register Now</h2> {/* Reduced font size */}
-        <form>
+        <form onSubmit={handleLogin}> 
           <label htmlFor="name" className="block text-sm font-medium text-gray-700">
             Name
           </label>
-          <input type="name" id="name" placeholder="Enter your full name" className="w-full border border-gray-300 p-2 rounded-md mt-1 focus:outline-none" />
+          <input type="name" id="name" placeholder="Enter your full name"
+          onChange={(e)=> setUsername(e.target.value)}
+          className="w-full border border-gray-300 p-2 rounded-md mt-1 focus:outline-none" />
 
           <label htmlFor="email" className="block text-sm font-medium text-gray-700 mt-2">
             Email
           </label>
-          <input type="email" id="email" placeholder="Enter email" className="w-full border border-gray-300 p-2 rounded-md mt-1 focus:outline-none" />
+          <input type="email" id="email" placeholder="Enter email" 
+            onChange={(e)=> setEmail(e.target.value)}
+            className="w-full border border-gray-300 p-2 rounded-md mt-1 focus:outline-none" />
 
           <label htmlFor="password" className="block text-sm font-medium text-gray-700 mt-4">
             Password
           </label>
           <div className="relative">
-            <input type="password" id="password" placeholder="Enter Password" className="w-full border border-gray-300 p-2 rounded-md mt-1 focus:outline-none" />
+            <input type="password" id="password" placeholder="Enter Password"
+            onChange={(e)=> setPassword(e.target.value)}
+            className="w-full border border-gray-300 p-2 rounded-md mt-1 focus:outline-none" />
             <FaEye className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 cursor-pointer" />
           </div>
 
@@ -33,14 +66,18 @@ const Register: React.FC = () => {
             Confirm Password
           </label>
           <div className="relative">
-            <input type="password" id="password" placeholder="Enter Confirm Password" className="w-full border border-gray-300 p-2 rounded-md mt-1 focus:outline-none" />
+            <input type="password" id="password" placeholder="Enter Confirm Password"
+            onChange={(e)=> setConfirmPassword(e.target.value)}
+            className="w-full border border-gray-300 p-2 rounded-md mt-1 focus:outline-none" />
             <FaEye className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 cursor-pointer" />
           </div>
 
           <div className="text-right text-sm text-blue-500 mt-1 cursor-pointer">Forget Password?</div>
           <button type="submit" className="w-full bg-red-500 text-white p-2 rounded-md mt-4 hover:bg-red-600">
-            CONTINUE
+            {isLoading ? "Logging in..": "Continue"}
           </button>
+          {isError&&
+          <p>Error: {error && 'status' in error ? `Status ${error.status}` : error?.message}</p>}
         </form>
         <div className="text-center my-4 text-gray-500">Or Log in with</div>
         <button className="w-full bg-white border border-gray-300 text-black p-2 rounded-md hover:bg-gray-100 flex items-center justify-center">
