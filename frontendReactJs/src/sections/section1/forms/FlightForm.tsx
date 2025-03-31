bf9c93f7b28113909240e7e11cf164aebeceb58d
dipesh <dipeshpaharai@gmail.com>
2025-03-31 23:29:49 +0545

    basic ui update

--- frontendReactJs/src/sections/section1/forms/FlightForm.tsx
+++ frontendReactJs/src/sections/section1/forms/FlightForm.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
@@ -417,6 +418,17 @@ export const FlightForm: React.FC = () => {
             </div>
           ))}
         </RadioGroup>
+      </div>
+      {/* <div className="absolute mx-[480px] mt-6 "> */}
+      <div className=" w-full flex items-center justify-center   ">
+        <Link to="/results">
+          <button type="submit" className="mt-5 inline-flex items-center py-2.5 px-3 ms-2 text-sm font-bold text-black bg-[#FFF7D3] rounded-lg  hover:bg-white ">
+            <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+            Search
+          </button>
+        </Link>
       </div>
     </div>
   );
