6d322f893c069048f5df685edfcbf3fc222ec6a6
dipesh <dipeshpaharai@gmail.com>
2025-04-08 22:36:20 +0545

    backend Updated

--- backendSpringBoot/src/main/java/com/eastTrip/backendSpringBoot/config/SecurityConfig.java
+++ backendSpringBoot/src/main/java/com/eastTrip/backendSpringBoot/config/SecurityConfig.java
@@ -17,8 +17,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
+//import org.springframework.web.cors.reactive.CorsConfigurationSource;
 
 import java.util.List;
 
@@ -51,7 +52,7 @@ public class SecurityConfig {
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
-        return (CorsConfigurationSource) source;
+        return  source;
     }
 
     // Security Filter Chain configuration
