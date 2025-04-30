c490c2e1f7ba64f4138961a3f423944002fee2c4
dipesh <dipeshpaharai@gmail.com>
2025-04-30 10:23:22 +0545

    update

--- src/Components/ResultBox/Result3.jsx
+++ src/Components/ResultBox/Result3.jsx
@@ -61,7 +61,7 @@ export default function Result3() {
   return (
     <section className="page-content">
     <div className="hotel-listing-container">
-      <h1 className="hotel-listing-title">Showing Results for Homestays in {location}</h1>
+      <h1 className="hotel-listing-title">Showing Results for Homestayss in {location}</h1>
 
       <div className="hotel-cards-container">
         {currentHomeStays.map((hotel, index) => (
