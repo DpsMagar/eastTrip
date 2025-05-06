4364d3dedd339ebdfebbb031d5380561cd845759
Prajwal Limbu <np03cs4a230068@heraldcollege.edu.np>
2025-05-06 19:51:57 +0545

    homepage design  updated

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import Search from '../../Components/SearchBar/Search';
-import Box from '../../Components/Boxcard/BigBox'
-const Home = () => {
-    return (
-      <section className="page-content">
-        <Search />
-        <Box />
+import Box from '../../Components/Boxcard/BigBox';
+import './Home.css';
+import himal from '../../Assest/himal.png';
 
-      </section>
-    );
+const Home = () => {
+  return (
+    <section className="home-background">
+      <div className="home-wrapper">
+        <div className="hero-section">
+          <img src={himal} alt="Himalayas" className="hero-bg" />
+          <div className="search-overlay">
+            <Search />
+          </div>
+        </div>
+
+        <div className="home-content">
+          <Box />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
